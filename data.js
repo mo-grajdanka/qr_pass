00dad757e23f880e775621466ae6663b2c9e0bde
mo-grajdanka <pyrus.ipvvv@gmail.com>
2025-10-06 11:08:39 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+<script>
+
+  window.PROFILES = [
+    {
+      id: "u1",
+      arendator: "ООО «Ромашка»",
+      name: "Иванов Иван",
+      photo_url: "https://avatars.mds.yandex.net/i?id=d3528e3e83a323a8588658cc4f9e8271-5487054-images-thumbs&n=13",
+      zone: "Склад №1",
+      status: "active", // active | revoked
+      valid_from: "2025-10-06T06:00:00Z",
+      valid_to:   "2025-10-06T18:00:00Z"
+    },
+    {
+      id: "u2",
+      arendator: "ООО «Ромашка»",
+      name: "Петров Пётр",
+      photo_url: "https://avatars.mds.yandex.net/i?id=6dcbf4c7e81cefc4af6f4a5334658be0_l-9456276-images-thumbs&n=13",
+      zone: "Офис №3",
+      status: "revoked",
+      valid_from: "2025-10-06T06:00:00Z",
+      valid_to:   "2025-10-06T18:00:00Z"
+    }
+  ];
+</script>
